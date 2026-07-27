08a8e31eb4b6c1ecc739a586d3f5606ecbe07b23
Sameer Shemna <sameersemna@yahoo.com>
2026-07-28 01:36:03 +0200

    chore: update gitignore and test script
    - update .gitignore to ignore all response JSON files using a wildcard
    - update test.sh to use llama3.2:latest and add a new OpenRouter API test case
    - comment out old curl command in test.sh

--- test.sh
+++ test.sh
@@ -6,14 +6,26 @@
 # qwen3.6:27b-q8_0 minimax-m3:cloud minimax-m3:cloud llama3.2:latest llama3.2:1b
 # Use MCP Google Search Weather to answer: What is the weather forecast for this week? Please search for current conditions.
 
+# curl -X POST http://localhost:11410/v1/chat/completions \
+#   -H "Content-Type: application/json" \
+#   -d '{
+#     "model": "llama3.2:1b",
+#     "messages": [
+#       {
+#         "role": "user",
+#         "content": "More MCPs Added, refresh MCP Tools. List all the MCP Tools in details. What are the commands you have access to in a list."
+#       }
+#     ]
+#   }' > response.json
+
 curl -X POST http://localhost:11410/v1/chat/completions \
   -H "Content-Type: application/json" \
   -d '{
-    "model": "llama3.2:1b",
+    "model": "llama3.2:latest",
     "messages": [
       {
         "role": "user",
-        "content": "More MCPs Added, refresh MCP Tools. List all the MCP Tools in details. What are the commands you have access to in a list."
+        "content": "Use MCP sequential-thinking, memory and noapi-google-search to answer: What is the weather forecast for this week for Paris, France? Please search for current conditions. Also list other tools available to you from MCP google-search. List all the tools available to you from MCP google-search in a list."
       }
     ]
   }' > response.json
@@ -59,3 +71,18 @@ cat response.json | jq --raw-output '.choices[0].message.content'
     #     "STITCH_API_KEY": "API_KEY"
     #   }
     # }
+
+# sk-or-v1-REDACTED
+export OPENROUTER_API_KEY='sk-or-v1-REDACTED'
+curl https://openrouter.ai/api/v1/chat/completions \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer sk-or-v1-REDACTED" \
+  -d '{
+  "model": "openai/gpt-4o",
+  "messages": [
+    {
+      "role": "user",
+      "content": "What is the meaning of life?"
+    }
+  ]
+}'
